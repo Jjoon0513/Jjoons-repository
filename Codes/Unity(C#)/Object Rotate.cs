98065bd95aabd51652e98c1b9cd6ca79ba7a7a5a
쭌이Jjoon <82186614+Jjoon0513@users.noreply.github.com>
2023-03-14 21:28:06 +0900

    Update Object Rotate.cs

--- Codes/Unity(C#)/Object Rotate.cs
+++ Codes/Unity(C#)/Object Rotate.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     void Update()
+    //한틱마다 업데이트됩니다.
     {
+        //Time.deltaTime 유니티 고유변수를 이용하여 transform.Rotate변수를 바꿔줍니다.
         transform.Rotate(new Vector3(10f, 20f, 30f) * Time.deltaTime);
-
+    
         transform.Rotate(new Vector3(10f, 20f, 30f) * Time.deltaTime, Space.World);
     }
 }
