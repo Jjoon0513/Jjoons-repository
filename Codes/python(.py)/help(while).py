79680dad93278625c0ce357285f7500ecfea1ca4
쭌이Jjoon <82186614+Jjoon0513@users.noreply.github.com>
2023-03-15 15:56:00 +0900

    Update help(while).py

--- Codes/python(.py)/help(while).py
+++ Codes/python(.py)/help(while).py
@@ -5,30 +5,30 @@
 #while(참,거짓 변수):
 #    //실행문//
 #입니다
-while(x):
+while x:
   print("HelloWorld!")
  
   #x가 참인동안 HelloWorld!를 출력합니다
 
 #이를 이용하여 계속 반복하게 할 수 있습니다
 
-while(True):
+while True:
   print("HelloWorld!")
  
   #이는 HelloWorld를 계속해서 출력합니다
   #하지만 무한루프는 가급적 사용하지 않는게 좋습니다.
   
-while(x < 10):
+while x < 10:
   print("HelloWorld!")
   
   #x가 10 미만인동안 HelloWorld! 를 출력합니다
  
-while(x <= y):
+while x <= y:
   print("HelloWorld!")
   
   #x가 y 이하인 동안 HelloWorld! 를 출력합니다
   
-while(azzi.issleep()):
+while azzi.issleep():
   print("HelloWorld!")
   
   #azzi.issleep()이 참인동안 HelloWorld! 를 출력합니다 
